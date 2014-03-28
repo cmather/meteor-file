90d64a41a1b9fdea9b12370c15fbcb0151c42bb2
Christoph Herbst <chris.p.herbst@gmail.com>
2014-03-28 09:46:57 +0100

    remove ui helpers
    not compatible with the new blaze engine

--- package.js
+++ package.js
@@ -4,9 +4,7 @@ Package.describe({
 
 Package.on_use(function (api) {
   api.use(["underscore", "ejson"], ["client", "server"]);
-  api.use(["handlebars", "spark"], "client");
   api.add_files(["meteor-file.js"], ["client", "server"]);
-  api.add_files("meteor-file-uploader.js", "client");
 
   if (typeof api.export !== 'undefined') {
     api.export("MeteorFile", ["client", "server"]);
